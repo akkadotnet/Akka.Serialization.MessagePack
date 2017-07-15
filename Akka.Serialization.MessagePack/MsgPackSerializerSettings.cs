fe55a4ca4866612624cf97253e08bb1ea2460eed
Alex Valuyskiy <alexvaluyskiy@users.noreply.github.com>
2017-07-15 12:39:14 +0300

    Added Lz4 support (#12)

---
+++ Akka.Serialization.MessagePack/MsgPackSerializerSettings.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MsgPackSerializerSettings.cs" company="Akka.NET Project">
+//     Copyright (C) 2017 Akka.NET Contrib <https://github.com/AkkaNetContrib/Akka.Serialization.MessagePack>
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using Akka.Configuration;
+
+namespace Akka.Serialization.MessagePack
+{
+    public class MsgPackSerializerSettings
+    {
+        public MsgPackSerializerSettings(bool enableLz4Compression)
+        {
+            EnableLz4Compression = enableLz4Compression;
+        }
+
+        public bool EnableLz4Compression { get; }
+
+        public static readonly MsgPackSerializerSettings Default = new MsgPackSerializerSettings(
+            enableLz4Compression: false);
+
+        public static MsgPackSerializerSettings Create(Config config)
+        {
+            if (config == null) throw new ArgumentNullException(nameof(config), "MsgPackSerializerSettings require a config, default path: `akka.serializers.msgpack`");
+
+            return new MsgPackSerializerSettings(
+                enableLz4Compression: config.GetBoolean("enable-lz4-compression", false));
+        }
+    }
+}
